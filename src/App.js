13d4d82dd02c8239217d3cdb84c36c3e61f36c4f
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-22 17:08:25 -0600

    add banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+
 import Header from "./componet/Header";
 import Home from "./componet/Home";
 
--- src/componet/Home.js
+++ src/componet/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
+
+import Banner from "./Banner";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
       <h1>Home component</h1>
+      <Banner />
     </div>
   );
 }
