63b3414451ddd9af4c2105cd43d0d259e36a0593
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-24 13:06:10 -0600

    create 2nd rows card and footer

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import "./App.css";
 
 import Header from "./componet/Header";
 import Home from "./componet/Home";
+import Footer from "./componet/Footer";
 
 function App() {
   return (
     <div className="app">
-      {/* Hearder */}
       <Header />
-      {/* Home */}
       <Home />
-
-      {/* Banner */}
-      {/* Search */}
-
-      {/* Main - cards */}
-
-      {/* Footer */}
-
-      {/* SearchPage */}
+      <Footer />
     </div>
   );
 }
--- src/componet/Home.js
+++ src/componet/Home.js
@@ -26,9 +26,24 @@ function Home() {
         />
       </div>
       <div className="home_section">
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+          title="3 Bedroom Flat in Bournemouth"
+          description="Superhost with a stunning view of the beachsde in Sunny Bournemouth"
+          price="£130/night"
+        />
+        <Card
+          src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+          title="Penthouse in London"
+          description="Enjoy the amazing sights of London with this stunning penthouse"
+          price="£350/night"
+        />
+        <Card
+          src="https://media.nomadicmatt.com/2018/apartment.jpg"
+          title="1 Bedroom aprtment"
+          description="Superhost with great amenities and a fabolous shopping complex nearby"
+          price="£70/night"
+        />
       </div>
     </div>
   );
