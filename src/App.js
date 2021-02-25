2873d2662992868b3dbadac06e4da70338e9f358
estherkimyunjung <estherkimyunjung@gmail.com>
2021-02-25 11:43:05 -0600

    create search result page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import Header from "./componet/Header";
 import Home from "./componet/Home";
+import SearchPage from "./componet/SearchPage";
 import Footer from "./componet/Footer";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
